e66ab06af960928b7757f623fa1fd34345db4b9d
ThisRabbit <1195398892@qq.com>
2021-10-23 22:33:48 +0800

    front: feat: intruducing react and antd for UI

---
+++ frontend/enre-marker/src/webview/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Button } from 'antd';
+
+ReactDOM.render(
+  <Button>Hello</Button>,
+  document.getElementById('root') as HTMLElement
+);
